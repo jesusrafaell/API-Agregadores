a009a2dfee7e5cb02a9c85e65df223f8479a1388
Jesus R. Hernandez Vera <jhernandez@csc.tranred.com.ve>
2023-02-06 12:04:29 -0400

    remove

--- src/abono/abono.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AbonoService } from './abono.service';
-
-@Module({
-  providers: [AbonoService],
-  exports: [AbonoService],
-})
-export class AbonoModule {}
