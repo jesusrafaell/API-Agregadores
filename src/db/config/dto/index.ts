e3666b605b15ae4a61483d9e05baa7383fb5dbd2
Jesus R. Hernandez Vera <jhernandez@csc.tranred.com.ve>
2023-02-06 10:55:33 -0400

    2/6

--- src/db/config/dto/index.ts
+++
@@ -1,5 +0,0 @@
-import { DataSource } from 'typeorm';
-
-export interface IAgregadoresDS {
-  [key: number]: DataSource;
-}
--- src/db/contents/afiliados/afi_carropago.ts
+++
@@ -1,5 +0,0 @@
-import AfiliadosApi from '../../models/afiliados_api.entity';
-
-const list_afi_carropago: AfiliadosApi[] = [{ afiliado: '000000720008177' }];
-
-export default list_afi_carropago;
